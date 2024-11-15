15b0941acbd1a116564436e7e076756af9431289
adrianprzcb <adrianpc4444@gmail.com>
2024-11-16 00:03:04 +0100

    m

--- SpringBootWeb/springboot-web/src/main/java/com/adrian/springboot_web/controllers/UserController.java
+++ SpringBootWeb/springboot-web/src/main/java/com/adrian/springboot_web/controllers/UserController.java
@@ -1,8 +1,14 @@
 package com.adrian.springboot_web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+
+    @GetMapping("/details")
+    public String details(){
+        return "details";
+    }
 
 }
