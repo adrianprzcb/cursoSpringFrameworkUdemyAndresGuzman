c027ea4ae5f17ae8e18c2edd5f6a6c9bdf0f0771
adrianprzcb <adrianpc4444@gmail.com>
2024-11-16 00:26:10 +0100

    m

--- SpringBootWeb/springboot-web/src/main/java/com/adrian/springboot_web/controllers/UserController.java
+++ SpringBootWeb/springboot-web/src/main/java/com/adrian/springboot_web/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.adrian.springboot_web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
+    public String details(Model model) {
+        model.addAttribute("title", "Spring Boot App");
+        model.addAttribute("name", "Adrian");
+        model.addAttribute("lastname", "Gonzalez");
         return "details";
     }
 
