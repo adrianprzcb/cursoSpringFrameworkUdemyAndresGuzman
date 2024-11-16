98fd1447a962a9fc89adc7dcd6862a831a551295
adrianprzcb <adrianpc4444@gmail.com>
2024-11-16 16:35:52 +0100

    m

--- SpringBootWeb/springboot-web/src/main/java/com/adrian/springboot_web/controllers/UserController.java
+++ SpringBootWeb/springboot-web/src/main/java/com/adrian/springboot_web/controllers/UserController.java
@@ -1,7 +1,11 @@
 package com.adrian.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.adrian.springboot_web.models.User;
@@ -15,6 +19,15 @@ public class UserController {
         model.addAttribute("title", "Spring Boot App");
        model.addAttribute("user", user);
         return "details";
+    }
+
+
+    @GetMapping("/list")
+    public String list(ModelMap model){
+        List<User> users = new ArrayList<>();
+        model.addAttribute("users", users);
+        model.addAttribute("title", "Spring Boot App");
+        return "list";
     }
 
 }
