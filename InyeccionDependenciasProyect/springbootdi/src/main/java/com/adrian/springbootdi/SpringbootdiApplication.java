c84020a21be7b43dbe934fbf7b4696edfd6815cc
adrianprzcb <adrianpc4444@gmail.com>
2024-11-19 15:50:24 +0100

    di

---
+++ InyeccionDependenciasProyect/springbootdi/src/main/java/com/adrian/springbootdi/SpringbootdiApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.springbootdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdiApplication.class, args);
+	}
+
+}
---
+++ InyeccionDependenciasProyect/springbootdi/src/test/java/com/adrian/springbootdi/SpringbootdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrian.springbootdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
