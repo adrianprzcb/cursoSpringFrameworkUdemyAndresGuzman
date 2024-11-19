c05bf0f251cfef26b76d786c0fd5d845a20de606
adrianprzcb <adrianpc4444@gmail.com>
2024-11-19 11:39:27 +0100

    m

--- SpringBootWeb/springboot-web/src/main/java/com/adrian/springboot_web/controllers/PathVariableController.java
+++ SpringBootWeb/springboot-web/src/main/java/com/adrian/springboot_web/controllers/PathVariableController.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.adrian.springboot_web.models.User;
 import com.adrian.springboot_web.models.DTO.ParamDto;
 
 @RestController
@@ -32,5 +35,13 @@ public class PathVariableController{
 
         return json;
     }
+
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user){
+        
+        return user;
+    }
+
 
 }
