2994666214988d28e6c0e1eef3d3f34f5b8b8a8b
adrianprzcb <adrianpc4444@gmail.com>
2024-11-19 12:02:42 +0100

    m

--- SpringBootWeb/springboot-web/src/main/java/com/adrian/springboot_web/SpringbootWebApplication.java
+++ SpringBootWeb/springboot-web/src/main/java/com/adrian/springboot_web/SpringbootWebApplication.java
@@ -2,8 +2,6 @@ package com.adrian.springboot_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 public class SpringbootWebApplication {
