e472477cfcf2bbf75032d70045d8a4885d8d2dce
adrianprzcb <adrianpc4444@gmail.com>
2024-11-19 11:31:37 +0100

    m

--- SpringBootWeb/springboot-web/src/main/java/com/adrian/springboot_web/controllers/PathVariableController.java
+++ SpringBootWeb/springboot-web/src/main/java/com/adrian/springboot_web/controllers/PathVariableController.java
@@ -1,5 +1,8 @@
 package com.adrian.springboot_web.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +22,15 @@ public class PathVariableController{
         return param;
     }
     
+    @GetMapping("/mix/{product}/{code}")
+    public Map<String, Object> mixPathVar(@PathVariable String product,
+    @PathVariable Long code){
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("code",code);
+
+        return json;
+    }
 
 }
