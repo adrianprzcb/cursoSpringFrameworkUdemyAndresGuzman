d5c9edda205aa28ea3905d1153973352cd507c5b
adrianprzcb <adrianpc4444@gmail.com>
2024-11-20 16:35:07 +0100

    di

--- Facturas Ejemplo/ejemplofactura/src/main/java/com/adrian/ejemplofactura/models/Client.java
+++ Facturas Ejemplo/ejemplofactura/src/main/java/com/adrian/ejemplofactura/models/Client.java
@@ -2,4 +2,24 @@ package com.adrian.ejemplofactura.models;
 
 public class Client {
 
+    private String name;
+    private String lastname;
+
+
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    
+
 }
--- Facturas Ejemplo/ejemplofactura/src/main/java/com/adrian/ejemplofactura/models/Product.java
+++ Facturas Ejemplo/ejemplofactura/src/main/java/com/adrian/ejemplofactura/models/Product.java
@@ -2,4 +2,20 @@ package com.adrian.ejemplofactura.models;
 
 public class Product {
 
+    private String name;
+    private String price;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPrice() {
+        return price;
+    }
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
 }
