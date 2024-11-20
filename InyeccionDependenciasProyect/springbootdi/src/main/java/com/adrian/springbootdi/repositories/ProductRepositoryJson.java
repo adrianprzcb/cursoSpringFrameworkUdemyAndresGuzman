9e241f1cd502242a115fc87aa85f86e4e123674d
adrianprzcb <adrianpc4444@gmail.com>
2024-11-20 15:30:15 +0100

    di

---
+++ InyeccionDependenciasProyect/springbootdi/src/main/java/com/adrian/springbootdi/repositories/ProductRepositoryJson.java
@@ -0,0 +1,37 @@
+package com.adrian.springbootdi.repositories;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ClassPathResource;
+
+import com.adrian.springbootdi.models.Product;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ProductRepositoryJson implements ProductRepository{
+
+    List<Product> list;
+
+    public ProductRepositoryJson() {
+        Resource resource = new ClassPathResource("json/products.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return list;
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return null;
+    }
+
+}
