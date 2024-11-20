5e2ae03e5da688df2d06745bf727ebb4e07bbd5c
adrianprzcb <adrianpc4444@gmail.com>
2024-11-20 22:53:43 +0100

    em

--- Manejo Errores/springboot-errors/src/main/java/com/adrian/springboot_errors/controllers/HandlerExceptionController.java
+++ Manejo Errores/springboot-errors/src/main/java/com/adrian/springboot_errors/controllers/HandlerExceptionController.java
@@ -3,6 +3,7 @@ package com.adrian.springboot_errors.controllers;
 import java.util.Date;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,7 +20,8 @@ public class HandlerExceptionController {
         error.setError("Error division por cero");
         error.setMessage(ex.getMessage());
         error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        return ResponseEntity.internalServerError().body(error);
+    //    return ResponseEntity.internalServerError().body(error);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
     }
 
 }
