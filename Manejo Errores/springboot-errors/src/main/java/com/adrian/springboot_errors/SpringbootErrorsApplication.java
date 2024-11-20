b74117ee7a22de5e0ab4db39c4a58a5c9e8ca61e
adrianprzcb <adrianpc4444@gmail.com>
2024-11-20 21:20:34 +0100

    me

---
+++ Manejo Errores/springboot-errors/src/main/java/com/adrian/springboot_errors/SpringbootErrorsApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.springboot_errors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootErrorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootErrorsApplication.class, args);
+	}
+
+}
---
+++ Manejo Errores/springboot-errors/src/test/java/com/adrian/springboot_errors/SpringbootErrorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrian.springboot_errors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootErrorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
