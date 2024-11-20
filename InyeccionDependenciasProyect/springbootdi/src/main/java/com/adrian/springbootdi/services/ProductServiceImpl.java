bee72d01e433422c0167e8fe12145807aa7d33f1
adrianprzcb <adrianpc4444@gmail.com>
2024-11-20 14:58:55 +0100

    di

--- InyeccionDependenciasProyect/springbootdi/src/main/java/com/adrian/springbootdi/services/ProductServiceImpl.java
+++ InyeccionDependenciasProyect/springbootdi/src/main/java/com/adrian/springbootdi/services/ProductServiceImpl.java
@@ -3,7 +3,9 @@ package com.adrian.springbootdi.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.adrian.springbootdi.models.Product;
@@ -12,6 +14,8 @@ import com.adrian.springbootdi.repositories.ProductRepository;
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    @Autowired
+    private Environment environment;
     
     private ProductRepository repository;
 
@@ -22,7 +26,7 @@ public class ProductServiceImpl implements ProductService {
 
     public List<Product> findAll(){
         return repository.findAll().stream().map(p -> {
-            Double priceTax = p.getPrice() * 1.25d;
+            Double priceTax = p.getPrice() * environment.getProperty("config.price.tax", Double.class);
          //   Product newProduct = new Product(p.getId(), p.getName(), priceImp.longValue());
            Product newProduct = (Product) p.clone();
            newProduct.setPrice(priceTax.longValue());
