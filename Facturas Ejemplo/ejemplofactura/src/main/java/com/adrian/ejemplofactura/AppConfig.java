fae9c986abc8ac69a8a614e7b6c75ac59398a613
adrianprzcb <adrianpc4444@gmail.com>
2024-11-20 19:43:22 +0100

    di

--- Facturas Ejemplo/ejemplofactura/src/main/java/com/adrian/ejemplofactura/AppConfig.java
+++ Facturas Ejemplo/ejemplofactura/src/main/java/com/adrian/ejemplofactura/AppConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 import com.adrian.ejemplofactura.models.Item;
@@ -19,6 +20,15 @@ public class AppConfig {
         Product p1 = new Product("Laptop", 1500);
         Product p2 = new Product("Mouse", 20);
       return Arrays.asList(new Item(p1, 2), new Item(p2, 4));
+    }
+
+    @Bean
+    @Primary
+    List<Item> itemsInvoiceOffice(){
+        Product p1 = new Product("Camara", 1000);
+        Product p2 = new Product("Mousrerer", 32);
+        Product p3 = new Product("aewew", 12);
+      return Arrays.asList(new Item(p1, 1), new Item(p2, 3), new Item(p3, 5));
     }
     
 }
