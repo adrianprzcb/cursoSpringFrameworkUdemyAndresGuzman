741487933c7f81d46f2dbda18f53e11a6284c9ed
adrianprzcb <adrianpc4444@gmail.com>
2024-11-21 11:53:18 +0100

    eh

--- Manejo Errores/springboot-errors/src/main/java/com/adrian/springboot_errors/controllers/HandlerExceptionController.java
+++ Manejo Errores/springboot-errors/src/main/java/com/adrian/springboot_errors/controllers/HandlerExceptionController.java
@@ -10,13 +10,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.adrian.springboot_errors.models.Error;
 
 import io.micrometer.core.ipc.http.HttpSender.Response;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class HandlerExceptionController {
 
     @ExceptionHandler(NumberFormatException.class)
@@ -31,7 +32,7 @@ public class HandlerExceptionController {
         return error;
     }
 
-    
+
     @ExceptionHandler(ArithmeticException.class)
     public ResponseEntity<Error> divisionByZero(Exception ex){
         Error error = new Error();
