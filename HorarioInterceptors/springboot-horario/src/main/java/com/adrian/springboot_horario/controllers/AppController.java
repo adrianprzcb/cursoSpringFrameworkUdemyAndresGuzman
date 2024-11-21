c4fc37a8a5dc58bbd5a1aa55b51e4fc100771d46
adrianprzcb <adrianpc4444@gmail.com>
2024-11-21 17:57:09 +0100

    i

--- HorarioInterceptors/springboot-horario/src/main/java/com/adrian/springboot_horario/controllers/AppController.java
+++ HorarioInterceptors/springboot-horario/src/main/java/com/adrian/springboot_horario/controllers/AppController.java
@@ -9,14 +9,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class AppController {
 
     @GetMapping("/foo")
-    public ResponseEntity<?> foo(){
+    public ResponseEntity<?> foo(HttpServletRequest request){
         Map<String, Object> data = new HashMap<>();
         data.put("Title", "Bienvenido al sistema de atención");
         data.put("Date", new Date());
+        data.put("message", request.getAttribute("message"));
         return ResponseEntity.ok(data);
     }
 
