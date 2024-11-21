180e2009f98b4c086340322a5287b3df58e53184
adrianprzcb <adrianpc4444@gmail.com>
2024-11-21 23:09:28 +0100

    AOP

---
+++ AOP/springboot-aop/src/main/java/com/adrian/springboot_aop/aop/GreetingFooAspect.java
@@ -0,0 +1,44 @@
+package com.adrian.springboot_aop.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(1)
+@Component
+@Aspect
+public class GreetingFooAspect {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+      @Before("execution(* com.adrian.springboot_aop.services.GreetingService.*(..))")
+    public void loggerBefore(JoinPoint joinPoint){
+
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("XAntes: " + method + " invocado con los parametros " + args);
+    }
+
+
+    @After("execution(* com.adrian.springboot_aop.services.GreetingService.*(..))")
+    public void loggerAfter(JoinPoint joinPoint){
+
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("XDespues: " + method + " con los argumentos " + args);
+    }
+
+
+      
+}
