c1960e196b1dbe36291d1090ba729ff4d5cadcd3
adrianprzcb <adrianpc4444@gmail.com>
2024-11-21 16:10:31 +0100

    http i

--- HttpInterceptors/springboot-interceptor/src/main/java/com/adrian/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ HttpInterceptors/springboot-interceptor/src/main/java/com/adrian/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,7 @@
 package com.adrian.springboot_interceptor.interceptors;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
@@ -18,21 +20,32 @@ public class LoadingTimeInterceptor implements HandlerInterceptor{
 
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-            @Nullable ModelAndView modelAndView) throws Exception {
-        logger.info("LoadingTimeInterceptor: postHandle() entrando ...." + ((HandlerMethod) handler).getMethod().getName());
-    }
-
-    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
             HandlerMethod controller = ((HandlerMethod) handler);
             logger.info("LoadingTimeInterceptor: preHandle() entrando ...." + controller.getMethod().getName());
 
+
+            long start = System.currentTimeMillis();
+            request.setAttribute("start", start);
+
+            Random random = new Random();
+            int delay= random.nextInt(500);
+            Thread.sleep(delay);
+
         return true;
     }
 
 
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+                long end = System.currentTimeMillis();
+                long start = (long) request.getAttribute("start");
+                long result = end - start;
+                logger.info("Tiempo transcurrido: " + result + " milisegundos");
+        logger.info("LoadingTimeInterceptor: postHandle() entrando ...." + ((HandlerMethod) handler).getMethod().getName());
+    }
     
 
 }
