7a51bf30b9059216974561a4241cb3056db01742
adrianprzcb <adrianpc4444@gmail.com>
2024-11-21 11:52:24 +0100

    eh

--- Manejo Errores/springboot-errors/src/main/java/com/adrian/springboot_errors/controllers/HandlerExceptionController.java
+++ Manejo Errores/springboot-errors/src/main/java/com/adrian/springboot_errors/controllers/HandlerExceptionController.java
@@ -1,19 +1,37 @@
 package com.adrian.springboot_errors.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.adrian.springboot_errors.models.Error;
 
+import io.micrometer.core.ipc.http.HttpSender.Response;
+
 @ControllerAdvice
 public class HandlerExceptionController {
 
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> numberFormatException(Exception e){
+        Map<String, Object> error = new HashMap<>();
+        error.put("date", new Date().toString());
+        error.put("error", "Número invadlido o incorrecto, no tiene formato de digito!");
+        error.put("message" , e.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        
+        return error;
+    }
+
+    
     @ExceptionHandler(ArithmeticException.class)
     public ResponseEntity<Error> divisionByZero(Exception ex){
         Error error = new Error();
@@ -37,5 +55,8 @@ public class HandlerExceptionController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
 
     }
+
+
+
 
 }
