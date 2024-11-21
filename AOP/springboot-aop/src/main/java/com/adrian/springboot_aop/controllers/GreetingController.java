9a0e6beb8328ab636ed155ed4faa6eed29f1e592
adrianprzcb <adrianpc4444@gmail.com>
2024-11-21 21:47:21 +0100

    aop

---
+++ AOP/springboot-aop/src/main/java/com/adrian/springboot_aop/controllers/GreetingController.java
@@ -0,0 +1,24 @@
+package com.adrian.springboot_aop.controllers;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adrian.springboot_aop.services.GreetingService;
+
+@RestController
+public class GreetingController {
+
+    @Autowired
+    private GreetingService greeting;
+
+    @GetMapping("/greeting")
+    public ResponseEntity<?> greeting(){
+        return ResponseEntity.ok(Collections.singletonMap("greeting", greeting.sayHello("Pepe", "Hola que tal!")));
+    }
+
+}
---
+++ AOP/springboot-aop/src/main/java/com/adrian/springboot_aop/services/GreetingService.java
@@ -0,0 +1,7 @@
+package com.adrian.springboot_aop.services;
+
+public interface GreetingService {
+
+    String sayHello(String person, String phrase);
+    
+}
