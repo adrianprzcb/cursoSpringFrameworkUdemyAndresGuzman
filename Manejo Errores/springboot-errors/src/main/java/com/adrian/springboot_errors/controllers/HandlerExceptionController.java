66183b3a3bae2651511e1eececfb24788910f5c9
adrianprzcb <adrianpc4444@gmail.com>
2024-11-21 11:15:04 +0100

    eh

--- Manejo Errores/springboot-errors/src/main/java/com/adrian/springboot_errors/controllers/HandlerExceptionController.java
+++ Manejo Errores/springboot-errors/src/main/java/com/adrian/springboot_errors/controllers/HandlerExceptionController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.adrian.springboot_errors.models.Error;
 
@@ -22,6 +23,19 @@ public class HandlerExceptionController {
         error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
     //    return ResponseEntity.internalServerError().body(error);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFoundException(NoHandlerFoundException e){
+
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Api Rest no encontrado");
+        error.setMessage(e.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+
     }
 
 }
