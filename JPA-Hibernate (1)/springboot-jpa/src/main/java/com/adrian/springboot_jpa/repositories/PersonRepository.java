32e97616f54fec8e305fcbdff4e1aafa409320ef
adrianprzcb <adrianpc4444@gmail.com>
2024-11-22 12:48:02 +0100

    jpa

--- JPA-Hibernate (1)/springboot-jpa/src/main/java/com/adrian/springboot_jpa/repositories/PersonRepository.java
+++ JPA-Hibernate (1)/springboot-jpa/src/main/java/com/adrian/springboot_jpa/repositories/PersonRepository.java
@@ -4,10 +4,20 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.adrian.springboot_jpa.entities.Person;
+
+import java.lang.StackWalker.Option;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
+
+
+    @Query("select p from Person p where p.id=?1")
+    Optional<Person> findOne(Long id);
+
+    @Query("select p from Person p where p.name=?1")
+    Optional<Person> findOneName(String name);
 
     List<Person> findByProgrammingLanguage(String programmingLanguage);
 
