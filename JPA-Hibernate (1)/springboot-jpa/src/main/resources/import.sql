10a808af02432883598c203be479047fa4bfb623
adrianprzcb <adrianpc4444@gmail.com>
2024-11-22 11:16:47 +0100

    jpa

---
+++ JPA-Hibernate (1)/springboot-jpa/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Alice', 'Smith', 'Java');
+
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Bob', 'Johnson', 'Python');
+
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Charlie', 'Brown', 'JavaScript');
+
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Diana', 'Martinez', 'C++');
+
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Ethan', 'Clark', 'Ruby');
