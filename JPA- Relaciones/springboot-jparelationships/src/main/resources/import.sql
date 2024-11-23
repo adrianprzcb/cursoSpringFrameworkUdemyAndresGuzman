f11281ce473d226b8e832e6a573c71ad93f41be7
adrianprzcb <adrianpc4444@gmail.com>
2024-11-23 14:36:02 +0100

    jpa

---
+++ JPA- Relaciones/springboot-jparelationships/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into client (name, lastname) values ("Pepe" , "Doe");
+insert into client (name, lastname) values ("John" , "Doe");
+insert into client (name, lastname) values ("Maria" , "Roe");
