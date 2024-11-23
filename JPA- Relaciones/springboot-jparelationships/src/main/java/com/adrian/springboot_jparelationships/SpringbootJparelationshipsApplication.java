c42a8b9711fd9797cbd9ed0e60199adb42e40d9f
adrianprzcb <adrianpc4444@gmail.com>
2024-11-23 14:46:27 +0100

    jpa

--- JPA- Relaciones/springboot-jparelationships/src/main/java/com/adrian/springboot_jparelationships/SpringbootJparelationshipsApplication.java
+++ JPA- Relaciones/springboot-jparelationships/src/main/java/com/adrian/springboot_jparelationships/SpringbootJparelationshipsApplication.java
@@ -1,13 +1,44 @@
 package com.adrian.springboot_jparelationships;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.adrian.springboot_jparelationships.entities.Client;
+import com.adrian.springboot_jparelationships.entities.Invoice;
+import com.adrian.springboot_jparelationships.repositories.ClientRepository;
+import com.adrian.springboot_jparelationships.repositories.InvoiceRepository;
+
 @SpringBootApplication
-public class SpringbootJparelationshipsApplication {
+public class SpringbootJparelationshipsApplication implements CommandLineRunner{
+
+	@Autowired
+	private InvoiceRepository invoiceRepository;
+
+	@Autowired
+	private ClientRepository clientRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJparelationshipsApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		ManyToOne();
+		
+	}
+
+	private void ManyToOne() {
+		Client client = new Client("Adrian", "Gonzalez");
+		clientRepository.save(client);
+
+		Invoice invoice = new Invoice("Compras de oficina", 2000L);
+		invoice.setClient(client);
+		Invoice invoiceDb = invoiceRepository.save(invoice);
+		System.out.println("Invoice saved: " + invoiceDb);
 	}
 
 }
