bfe7c47cce2649912e7d8d5c9a44f8ad763c3733
adrianprzcb <adrianpc4444@gmail.com>
2024-11-24 03:22:13 +0100

    jpa

---
+++ JPA- Relaciones/springboot-jparelationships/src/main/java/com/adrian/springboot_jparelationships/entities/ClientDetails.java
@@ -0,0 +1,62 @@
+package com.adrian.springboot_jparelationships.entities;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "client_details")
+public class ClientDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private boolean premium;
+    private Integer points;
+
+    @OneToOne
+    private Client client;
+
+    public ClientDetails() {
+    }
+
+    public ClientDetails(boolean premium, Integer points) {
+        this.premium = premium;
+        this.points = points;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isPremium() {
+        return premium;
+    }
+
+    public void setPremium(boolean premium) {
+        this.premium = premium;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientDetails{" + "id=" + id + ", premium=" + premium + ", points=" + points + '}';
+    }
+
+}
