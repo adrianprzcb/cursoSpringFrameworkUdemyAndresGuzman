a2233ccad6fb2c9cbc01165a43910af36dfdc7d0
adrianprzcb <adrianpc4444@gmail.com>
2024-11-24 19:14:08 +0100

    crud

--- JPA - CRUD/springboot-crud/src/main/java/com/adrian/springboot_crud/controllers/ProductController.java
+++ JPA - CRUD/springboot-crud/src/main/java/com/adrian/springboot_crud/controllers/ProductController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,4 +50,16 @@ public class ProductController {
     public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product){
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        Optional<Product> product = productService.findById(id);
+        Optional<Product> optProduct = productService.findById(id);
+        if(optProduct.isPresent()){
+          return ResponseEntity.ok(productService.delete(optProduct.get()));
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
