d93ad9315c9ebf3f03a68eb1eadc0fc1e711fd7d
adrianprzcb <adrianpc4444@gmail.com>
2024-11-24 20:12:00 +0100

    jpa

--- JPA - CRUD/springboot-crud/src/main/java/com/adrian/springboot_crud/controllers/ProductController.java
+++ JPA - CRUD/springboot-crud/src/main/java/com/adrian/springboot_crud/controllers/ProductController.java
@@ -1,12 +1,15 @@
 package com.adrian.springboot_crud.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,12 +48,18 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> create(@Valid @RequestBody Product product){
+    public ResponseEntity<Product> create(@Valid @RequestBody Product product, BindingResult result){
+        if(result.hasFieldErrors()){
+            return validation(result);
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Product> update(@PathVariable Long id, @Valid @RequestBody Product product){
+    public ResponseEntity<Product> update( @Valid @RequestBody Product product, BindingResult result, @PathVariable Long id){
+        if(result.hasFieldErrors()){
+            return validation(result);
+        }
         Optional<Product> optProduct = productService.update(id,product);
         if(optProduct.isPresent()){
             return ResponseEntity.status(HttpStatus.CREATED).body(optProduct.orElseThrow());
@@ -67,6 +76,14 @@ public class ProductController {
         }else{
             return ResponseEntity.notFound().build();
         }
+    }
+
+    private ResponseEntity<Product> validation(BindingResult result){
+        Map<String, String> errors = new HashMap<>();
+        result.getFieldErrors().forEach(err ->{
+            errors.put(err.getField(), "El campo " + err.getField() + " " + err.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().build();
     }
 
 }
