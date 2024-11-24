9526af4e403b6cd5c61a17ae99a328e85d309e40
adrianprzcb <adrianpc4444@gmail.com>
2024-11-24 23:42:09 +0100

    jpa

--- JPA - CRUD/springboot-crud/src/main/java/com/adrian/springboot_crud/ProductValidation.java
+++ JPA - CRUD/springboot-crud/src/main/java/com/adrian/springboot_crud/ProductValidation.java
@@ -2,6 +2,7 @@ package com.adrian.springboot_crud;
 
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import com.adrian.springboot_crud.models.entities.Product;
@@ -12,18 +13,24 @@ import com.adrian.springboot_crud.models.entities.Product;
 public class ProductValidation implements Validator {
     @Override
     public boolean supports(Class<?> clazz) {
-        return Product.class.equals(clazz);
+        return Product.class.isAssignableFrom(clazz);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
         Product product = (Product) target;
-        if (product.getName() == null || product.getName().isEmpty()) {
-            errors.rejectValue("name", "name.empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", null, "es requerido!");
+        // ValidationUtils.rejectIfEmptyOrWhitespace(errors, "description", "NotBlank.product.description");
+        if (product.getDescription() == null || product.getDescription().isBlank()) {
+            errors.rejectValue("description", null, "es requerido, por favor");
         }
-        if (product.getPrice() == null || product.getPrice() <= 0) {
-            errors.rejectValue("price", "price.invalid");
-        }
-    }
 
+        if (product.getPrice() == null) {
+            errors.rejectValue("price", null, "no puede ser nulo, ok!");
+        } else if (product.getPrice() < 500) {
+            errors.rejectValue("price", null, "debe ser un valor numerico mayor o igual que 500!");
+        }
+
+    }
+    
 }
