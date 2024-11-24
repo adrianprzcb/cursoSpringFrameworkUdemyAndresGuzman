a0d1283034a8e88ef3a2824c2ae14e3cf66f4c73
adrianprzcb <adrianpc4444@gmail.com>
2024-11-24 22:52:30 +0100

    crud

--- JPA - CRUD/springboot-crud/src/main/java/com/adrian/springboot_crud/ProductValidation.java
+++ JPA - CRUD/springboot-crud/src/main/java/com/adrian/springboot_crud/ProductValidation.java
@@ -1,5 +1,29 @@
 package com.adrian.springboot_crud;
 
-public class ProductValidation {
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.adrian.springboot_crud.models.entities.Product;
+
+
+
+@Component
+public class ProductValidation implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Product.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Product product = (Product) target;
+        if (product.getName() == null || product.getName().isEmpty()) {
+            errors.rejectValue("name", "name.empty");
+        }
+        if (product.getPrice() == null || product.getPrice() <= 0) {
+            errors.rejectValue("price", "price.invalid");
+        }
+    }
 
 }
