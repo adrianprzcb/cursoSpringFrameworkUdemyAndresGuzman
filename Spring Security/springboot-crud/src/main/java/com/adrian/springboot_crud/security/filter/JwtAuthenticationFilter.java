9a5d4106ae4d47ae35400fc839eef9100cdb3a15
adrianprzcb <adrianpc4444@gmail.com>
2024-11-25 15:00:42 +0100

    sec

--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/security/filter/JwtAuthenticationFilter.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/security/filter/JwtAuthenticationFilter.java
@@ -101,6 +101,16 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             response.setStatus(200);
     }
 
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException failed) throws IOException, ServletException {
+        
+        Map<String, String> body = new HashMap<>();
+        body.put("message", "Error de autenticación: username o password incorrecto");
+
+
+    }
+
 
     
     
