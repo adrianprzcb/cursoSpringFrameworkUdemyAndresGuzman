0063061ef5cd7bce63539e72f2e49cbd4889d431
adrianprzcb <adrianpc4444@gmail.com>
2024-11-25 13:33:27 +0100

    sec

--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/models/entities/Role.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/models/entities/Role.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +22,7 @@ public class Role {
     @Column(unique = true)
     private String name;
 
+    @ManyToMany(mappedBy = "roles")
     private List<User> users;
 
     public Role() {
@@ -46,6 +48,18 @@ public class Role {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
     }
 
 
