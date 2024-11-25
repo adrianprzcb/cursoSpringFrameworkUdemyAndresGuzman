493e85bda3532558c0664a0a30104f405b40f744
adrianprzcb <adrianpc4444@gmail.com>
2024-11-25 16:09:02 +0100

    sec

--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/security/SecurityConfig.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.adrian.springboot_crud.security;
 
-import java.net.Authenticator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
