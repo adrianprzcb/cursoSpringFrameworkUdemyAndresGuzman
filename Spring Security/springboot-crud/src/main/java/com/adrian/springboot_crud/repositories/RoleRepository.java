9c8756f6a203e569a271fe8a9c5b146a7f875330
adrianprzcb <adrianpc4444@gmail.com>
2024-11-25 11:38:58 +0100

    sec

--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/repositories/RoleRepository.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/repositories/RoleRepository.java
@@ -1,9 +1,11 @@
 package com.adrian.springboot_crud.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.adrian.springboot_crud.models.entities.Role;
 
+@Repository
 public interface RoleRepository extends CrudRepository<Role, Long>{
 
 }
