3aeba3d139b2cad61f7e8691b00d02f14921de40
adrianprzcb <adrianpc4444@gmail.com>
2024-11-25 21:51:59 +0100

    sec

--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/security/SecurityConfig.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/security/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.adrian.springboot_crud.security;
 
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,6 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.adrian.springboot_crud.security.filter.JwtAuthenticationFilter;
 import com.adrian.springboot_crud.security.filter.JwtValidationFilter;
@@ -60,7 +64,18 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOriginPatterns(null)
+        corsConfig.setAllowedOriginPatterns(Arrays.asList("*"));
+        corsConfig.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization","Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
     }
+
+
+    @Bean
+    FilterRegistrationBean
 
 }
