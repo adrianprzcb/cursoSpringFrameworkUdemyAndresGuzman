f81c91c7f89d580a377079c13ba175526e6cbf75
adrianprzcb <adrianpc4444@gmail.com>
2024-11-25 11:33:02 +0100

    sec

--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/models/entities/User.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/models/entities/User.java
@@ -1,10 +1,16 @@
 package com.adrian.springboot_crud.models.entities;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +24,54 @@ public class User {
 
     @Column(unique = true)
     private String username;
+
+    private String password;
+
+    @ManyToMany
+    @JoinTable(name = "users_roles", 
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"),
+    uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "role_id"}))
+    private List<Role> roles;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+    
+
+
+
 
 
 }
