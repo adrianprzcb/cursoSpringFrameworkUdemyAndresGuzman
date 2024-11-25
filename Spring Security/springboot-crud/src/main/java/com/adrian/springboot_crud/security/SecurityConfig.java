b26aeb280d2bddb568a61a4160ad73716cbe3e6b
adrianprzcb <adrianpc4444@gmail.com>
2024-11-25 12:08:26 +0100

    sec

--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/security/SecurityConfig.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/security/SecurityConfig.java
@@ -1,10 +1,16 @@
 package com.adrian.springboot_crud.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
 
-    
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 }
--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/services/implementations/UserServiceImpl.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/services/implementations/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.adrian.springboot_crud.services.implementations;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.adrian.springboot_crud.models.entities.Role;
 import com.adrian.springboot_crud.models.entities.User;
+import com.adrian.springboot_crud.repositories.RoleRepository;
 import com.adrian.springboot_crud.repositories.UserRepository;
 import com.adrian.springboot_crud.services.interfaces.UserService;
 
@@ -15,18 +20,36 @@ import com.adrian.springboot_crud.services.interfaces.UserService;
 public class UserServiceImpl implements UserService{
 
     @Autowired
-    UserRepository repository;
+    UserRepository userRepository;
+
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+
 
     @Override
     @Transactional(readOnly = true)
     public List<User> findAll() {
-        return (List<User>) repository.findAll();
+        return (List<User>) userRepository.findAll();
     }
 
     @Override
     @Transactional
     public User save(User user) {
-        return repository.save(user);
+        Optional<Role> optionalRoleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> roles = new ArrayList<>();
+
+        optionalRoleUser.ifPresent(roles::add);
+
+        if (user.isAdmin()) {
+            Optional<Role> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(roles::add);
+        }
+        return userRepository.save(user);
     }
 
 }
