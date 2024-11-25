358bd2a4d45172704e45f1e88cd39c85564a33be
adrianprzcb <adrianpc4444@gmail.com>
2024-11-25 12:17:31 +0100

    sec

--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/models/entities/User.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/models/entities/User.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.JoinColumn;
@@ -24,8 +26,11 @@ public class User {
     private Long id;
 
     @Column(unique = true)
+    @NotBlank
+    @Size(min = 4, max = 12)
     private String username;
 
+    @NotBlank
     private String password;
 
     @Transient
