871d950bc76e3db29f2eb7e68d3070cfa73af3bd
adrianprzcb <adrianpc4444@gmail.com>
2024-11-25 14:16:35 +0100

    sec

--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/security/filter/JwtAuthenticationFilter.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/security/filter/JwtAuthenticationFilter.java
@@ -1,8 +1,21 @@
 package com.adrian.springboot_crud.security.filter;
 
 
+import java.io.IOException;
+
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.adrian.springboot_crud.models.entities.User;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 
@@ -11,5 +24,35 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     public JwtAuthenticationFilter(AuthenticationManager authenticatorManager){
         this.authenticatorManager = authenticatorManager;
     }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        
+            User user = null;
+            String username = null;
+            String password = null;
+
+            try {
+                user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+                username = user.getUsername();
+                password = user.getPassword();
+            } catch (StreamReadException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (DatabindException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+
+            return authenticatorManager.authenticate(authenticationToken);
+    }
+    
 
 }
