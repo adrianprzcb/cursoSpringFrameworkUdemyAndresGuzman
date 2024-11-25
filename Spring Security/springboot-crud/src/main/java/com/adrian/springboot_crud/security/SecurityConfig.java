55da0bcd5e08619e89c87223c9ae95aa86832899
adrianprzcb <adrianpc4444@gmail.com>
2024-11-25 15:10:28 +0100

    sec

--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/security/SecurityConfig.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/security/SecurityConfig.java
@@ -1,16 +1,29 @@
 package com.adrian.springboot_crud.security;
 
+import java.net.Authenticator;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.adrian.springboot_crud.security.filter.JwtAuthenticationFilter;
+
 @Configuration
 public class SecurityConfig {
+
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder(){
@@ -24,6 +37,7 @@ public class SecurityConfig {
         .requestMatchers(HttpMethod.GET,"/api/users").permitAll()
         .requestMatchers(HttpMethod.POST,"/api/users/register").permitAll()
         .anyRequest().authenticated())
+        .addFilter(new JwtAuthenticationFilter(authenticationManager()))
         .csrf(config -> config.disable())
         .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .build();
