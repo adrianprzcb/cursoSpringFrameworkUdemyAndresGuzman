04354e0691c0f91ae142a7ef6341fb195625b9fe
adrianprzcb <adrianpc4444@gmail.com>
2024-11-25 12:24:47 +0100

    sec

--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/security/SecurityConfig.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.adrian.springboot_crud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,6 +13,16 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+
+    SecurityFilterChain filterChain(HttpSecurity http){
+
+        return http.authorizeHttpRequests( (auth) -> authz
+        .requestMatchers("/users").permitAll()
+        .anyRequest().authenticated())
+        .csrf(config -> config.disable())
+
     }
 
 }
