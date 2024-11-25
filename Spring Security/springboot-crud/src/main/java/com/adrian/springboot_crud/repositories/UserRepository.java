07415821e08a08f3991bc56726a66e346b6117ed
adrianprzcb <adrianpc4444@gmail.com>
2024-11-25 13:59:04 +0100

    sec

--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/repositories/UserRepository.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.adrian.springboot_crud.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.adrian.springboot_crud.models.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/services/implementations/JpaUserDetailsService.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/services/implementations/JpaUserDetailsService.java
@@ -1,15 +1,25 @@
 package com.adrian.springboot_crud.services.implementations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.adrian.springboot_crud.repositories.UserRepository;
+
+@Service
 public class JpaUserDetailsService implements UserDetailsService{
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
 }
