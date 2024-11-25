a8c315208b9eaaa2d433a213ee73a8926b167dfc
adrianprzcb <adrianpc4444@gmail.com>
2024-11-25 13:25:30 +0100

    sec

--- Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/controllers/UserController.java
+++ Spring Security/springboot-crud/src/main/java/com/adrian/springboot_crud/controllers/UserController.java
@@ -40,6 +40,13 @@ public class UserController {
     }
 
 
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@Valid @RequestBody User user, BindingResult result){
+        user.setAdmin(false);
+        return create(user, result);
+    }
+
+
       private ResponseEntity<?> validation(BindingResult result){
         Map<String, String> errors = new HashMap<>();
         result.getFieldErrors().forEach(err ->{
