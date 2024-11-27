f2ceab6d05441f6e2befb55a45abbf7a2dceb5b0
adrianprzcb <adrianpc4444@gmail.com>
2024-11-27 12:00:13 +0100

    react

--- Spring React App/react-app/src/components/ProductApp.jsx
+++ Spring React App/react-app/src/components/ProductApp.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react" 
 
 const initProducts = [
-    { name: 'Product 1', price: 100 },
-    {  name: 'Product 2', price: 200 },
-    { name: 'Product 3', price: 300 },
+    { name: 'Product 1', price: 100, description : 'fefefefe'  },
+    {  name: 'Product 2', price: 200, description : 'This is a 2e2eda' },
+    { name: 'Product 3', price: 300, description: 'This is a product' },
 ]
 
 
@@ -13,6 +13,22 @@ export const ProductApp = () => {
     return (
         <>
         <h1>Product App</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <thead>
+                {products.map(product => {
+                    <tr>
+                        <td> { product.name }</td>
+                    </tr>
+                })}
+            </thead>
+        </table>
         </>
     )
 }
