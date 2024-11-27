363d72e1075116f87c456f4ceb311e98bcefe20d
adrianprzcb <adrianpc4444@gmail.com>
2024-11-27 21:58:45 +0100

    react

--- Spring React App/react-app/src/components/ProductApp.jsx
+++ Spring React App/react-app/src/components/ProductApp.jsx
@@ -50,11 +50,9 @@ export const ProductApp = () => {
         <div className="container my-4">
             <h2>Productos</h2>
             <div className="row">
-
-            <div className="col">
+            <div >
                 <ProductForm handlerAdd = {handlerAddProduct} productSelected={productSelected}/>
             </div>
-
             <div> 
                 <ProductGrid products={products} handlerRemove={handlerRemoveProduct} handlerProductSelected={handlerProductSelected}/>
             </div>
