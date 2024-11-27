ba943dd3069275e5bad7f8319147070753402b8c
adrianprzcb <adrianpc4444@gmail.com>
2024-11-27 20:15:12 +0100

    react

--- Spring React App/react-app/src/components/ProductDetail.jsx
+++ Spring React App/react-app/src/components/ProductDetail.jsx
@@ -1,11 +1,14 @@
 import { PropTypes } from 'prop-types';
 
-export const ProductDetail = ({ handlerRemove, product = {}}) => {
+export const ProductDetail = ({ handlerProductSelected, handlerRemove, product = {}}) => {
     return(
         <tr>
                         <td> { product.name }</td>
                         <td> { product.price }</td>
                         <td> { product.description }</td>
+                        <td>
+                            <button onClick={() => handlerProductSelected(product)}>Update</button>
+                        </td>
                         <td>
                             <button onClick={() => handlerRemove(product.name)}>Remove</button>
                         </td>
@@ -16,5 +19,6 @@ export const ProductDetail = ({ handlerRemove, product = {}}) => {
 
 ProductDetail.propTypes = {
     product: PropTypes.object.isRequired,
-    handlerRemove: PropTypes.object.isRequired
+    handlerRemove: PropTypes.func.isRequired,
+    handlerProductSelected: PropTypes.fun.isRequired
 }
