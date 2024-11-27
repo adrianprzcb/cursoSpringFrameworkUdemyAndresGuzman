8f7fbb7a570a3b888ad03e1accfbd31bde1c2049
adrianprzcb <adrianpc4444@gmail.com>
2024-11-27 13:22:32 +0100

    react

---
+++ Spring React App/react-app/src/components/ProductForm.jsx
@@ -0,0 +1,24 @@
+
+export const ProductForm = () => {
+
+    return 
+    (
+        <form>
+            <input placeholder="Name"
+            name="name"
+            value={}
+            onChange={} 
+            />
+            <input placeholder="Description"
+            name="Description"
+            value={}
+            onChange={} 
+            />
+            <input placeholder="Price"
+            name="Price"
+            value={}
+            onChange={} 
+            />
+        </form>
+    )
+}
