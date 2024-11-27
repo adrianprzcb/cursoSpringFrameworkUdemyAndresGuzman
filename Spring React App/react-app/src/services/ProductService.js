34b327bc7ad55421b9192afeb893ab98f3f776a3
adrianprzcb <adrianpc4444@gmail.com>
2024-11-27 21:45:03 +0100

    react

--- Spring React App/react-app/src/services/ProductService.js
+++ Spring React App/react-app/src/services/ProductService.js
@@ -1,8 +1,8 @@
 
 const initProducts = [
-    { name: 'Product 1', price: 100, description : 'fefefefe'  },
-    {  name: 'Product 2', price: 200, description : 'This is a 2e2eda' },
-    { name: 'Product 3', price: 300, description: 'This is a product' },
+    { id: 1, name: 'Product 1', price: 100, description : 'fefefefe'  },
+    { id: 3, name: 'Product 2', price: 200, description : 'This is a 2e2eda' },
+    { id: 4, name: 'Product 3', price: 300, description: 'This is a product' },
 ]
 
 
