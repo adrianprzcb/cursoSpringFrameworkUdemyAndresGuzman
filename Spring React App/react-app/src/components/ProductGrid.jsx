7ea3be7122eef966c157c7c16ff60763974bc608
adrianprzcb <adrianpc4444@gmail.com>
2024-11-27 14:29:51 +0100

    react

--- Spring React App/react-app/src/components/ProductGrid.jsx
+++ Spring React App/react-app/src/components/ProductGrid.jsx
@@ -22,6 +22,6 @@ export const ProductGrid = ( {handlerRemove, products = []}) => {
 }
 
 ProductGrid.propTypes = {
-    products: PropTypes.array.isRequired
+    products: PropTypes.array.isRequired,
     handlerRemove: PropTypes.object.isRequired
 }
