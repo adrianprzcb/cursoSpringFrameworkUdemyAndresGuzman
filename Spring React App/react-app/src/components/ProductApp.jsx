6ab73e250db8ffa1b8c972b5c8365fa4844ce6c2
adrianprzcb <adrianpc4444@gmail.com>
2024-11-27 12:28:05 +0100

    react

--- Spring React App/react-app/src/components/ProductApp.jsx
+++ Spring React App/react-app/src/components/ProductApp.jsx
@@ -1,10 +1,5 @@
 import { useEffect, useState } from "react" 
 
-const initProducts = [
-    { name: 'Product 1', price: 100, description : 'fefefefe'  },
-    {  name: 'Product 2', price: 200, description : 'This is a 2e2eda' },
-    { name: 'Product 3', price: 300, description: 'This is a product' },
-]
 
 
 export const ProductApp = () => {
