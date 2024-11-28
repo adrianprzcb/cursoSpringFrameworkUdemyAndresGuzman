7e95e55a4b9153e7da02217ef0a53a86fa935d73
adrianprzcb <adrianpc4444@gmail.com>
2024-11-28 11:03:40 +0100

    back

--- Spring React App/backend/backend-products/src/main/java/com/adrian/backend_products/repositories/ProductRepository.java
+++ Spring React App/backend/backend-products/src/main/java/com/adrian/backend_products/repositories/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.adrian.backend_products.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.adrian.backend_products.entities.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
