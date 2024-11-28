1e328db3a52b99b5f31f2afcfbb9d8ed486450c3
adrianprzcb <adrianpc4444@gmail.com>
2024-11-29 00:03:15 +0100

    ang

--- Angular/frontend/angular-app/src/app/products/service/product.service.ts
+++ Angular/frontend/angular-app/src/app/products/service/product.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
+  const products: Product[]
 
   constructor() { }
 }
