3b1048ea64a0b1707cec858a824d22c6bbcbd484
adrianprzcb <adrianpc4444@gmail.com>
2024-11-28 20:13:43 +0100

    ang

--- Angular/frontend/angular-app/src/app/app.component.ts
+++ Angular/frontend/angular-app/src/app/app.component.ts
@@ -12,4 +12,9 @@ import { NgIf } from '@angular/common';
 export class AppComponent {
   title = 'Hola mundo angular 19';
   enabled: Boolean = true;
+
+
+  setEnabled(): void {
+    this.enabled = true ;
+  }
 }
