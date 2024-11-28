111e65de85f3fe3cf1fe187821dea6d05f986d55
adrianprzcb <adrianpc4444@gmail.com>
2024-11-28 13:10:09 +0100

    react

--- Spring React App/backend/backend-products/src/main/java/com/adrian/backend_products/repositories/ProductRepository.java
+++ Spring React App/backend/backend-products/src/main/java/com/adrian/backend_products/repositories/ProductRepository.java
@@ -2,9 +2,11 @@ package com.adrian.backend_products.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.adrian.backend_products.entities.Product;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RepositoryRestResource(path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
