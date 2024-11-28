f24f05b379362b0c32fb9a15b4ec1184846c3a72
adrianprzcb <adrianpc4444@gmail.com>
2024-11-28 12:54:19 +0100

    react

--- Spring React App/react-app/src/services/ProductService.js
+++ Spring React App/react-app/src/services/ProductService.js
@@ -14,7 +14,6 @@ export const listProducts = () => {
 const baseUrl = 'http://localhost:8080/products';
 
 export const findAll = async() => {
-
     try {
         const response = await axios.get(baseUrl);
         return response;
