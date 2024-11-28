b231929c43083ff6c36e12a983256dfe929109ad
adrianprzcb <adrianpc4444@gmail.com>
2024-11-28 20:56:47 +0100

    ang

--- Angular/frontend/angular-app/src/app/app.component.ts
+++ Angular/frontend/angular-app/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgIf],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
