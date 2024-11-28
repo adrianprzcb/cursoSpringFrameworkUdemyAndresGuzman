7cfb3cabe701a0b15ae696b310cf5c0412a305c4
adrianprzcb <adrianpc4444@gmail.com>
2024-11-28 11:00:31 +0100

    back

--- Spring React App/backend/backend-products/src/main/java/com/adrian/backend_products/entities/Product.java
+++ Spring React App/backend/backend-products/src/main/java/com/adrian/backend_products/entities/Product.java
@@ -1,6 +1,5 @@
 package com.adrian.backend_products.entities;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
