e74f0dcdb6cf36804bf4020af9168916cf563709
adrianprzcb <adrianpc4444@gmail.com>
2024-11-29 22:30:08 +0100

    ang

--- Angular/frontend/angular-app/src/app/products/components/product/product.component.ts
+++ Angular/frontend/angular-app/src/app/products/components/product/product.component.ts
@@ -49,7 +49,7 @@ export class ProductComponent implements OnInit{
         this.products = [... this.products, {...productNew}]
       }
       )
-      this.products.push(product)
+      this.productSelected = new Product();
     }
 
   }
