73008aae0aca9567b25a7081caeb2135aa5371cd
adrianprzcb <adrianpc4444@gmail.com>
2024-11-29 14:06:25 +0100

    ang

--- Angular/frontend/angular-app/src/main.ts
+++ Angular/frontend/angular-app/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { ProductComponent } from './app/products/components/product.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(ProductComponent, appConfig)
   .catch((err) => console.error(err));
