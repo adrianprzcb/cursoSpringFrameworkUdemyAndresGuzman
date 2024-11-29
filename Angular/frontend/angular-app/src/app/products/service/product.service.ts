0ab36e1ef7537a2a7e1420e33ff83491a2c7128e
adrianprzcb <adrianpc4444@gmail.com>
2024-11-29 22:10:57 +0100

    ang

--- Angular/frontend/angular-app/src/app/products/service/product.service.ts
+++ Angular/frontend/angular-app/src/app/products/service/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -29,6 +29,10 @@ export class ProductService {
   private url: string = 'http:localhost:8080/products'
 
   findAll(): Observable<Product []> {
-    return this.http.get<Product[]>(this.url)
+    return this.http.get<Product[]>(this.url).pipe(
+      map((response: any) =>
+        response._embedded.products as Product[]
+      ),
+    );
   }
 }
